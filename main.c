22907c6987ccdeb4bd009de8b75db8d220e63ea1
Michael Maurice <michaelmaurice92@gmail.com>
2019-05-05 00:31:17 +0000

    Function which zero's an integer using a pointer

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+void zeroInt(int *x);
+
+int main(){
+    int y = 10, i = 0, *x;
+    
+    i = y;
+    x = &y;
+    zeroInt(x);
+    printf("%d zero'd is %d\n", i, y);
+    
+}
+
+void zeroInt(int *x){
+    *x = 0; 
+}
