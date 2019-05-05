a0f62df34bed91e46c30d2fa388d9a7c6b82ed1d
Michael Maurice <michaelmaurice92@gmail.com>
2019-05-05 00:49:58 +0000

    function which uses pointers to swap two assigned integer values

--- main.c
+++ main.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 
 void zeroInt(int *x);
+void swap(int *a, int *b);
 
 int main(){
-    int y = 10, i = 0, *x;
+    int x = 100, y = 3, *a, *b, c, d;
     
-    i = y;
-    x = &y;
-    zeroInt(x);
-    printf("%d zero'd is %d\n", i, y);
+    a = &x;
+    b = &y;
+    c = x;
+    d = y;
+    swap(a, b);
+    printf("\n%d & %d swapped is %d & %d\n", c, d, x, y);
     
+    return 0;
 }
 
 void zeroInt(int *x){
     *x = 0; 
+}
+void swap(int *a, int *b){
+    int x, y;
+    
+    x = *a;
+    y = *b;
+    *a = y;
+    *b = x;
 }
