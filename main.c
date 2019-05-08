b6a864790e124f4e8d00a6ba442eeedf599ba73b
Michael Maurice <michaelmaurice92@gmail.com>
2019-05-08 03:16:54 +0000

    Did task 3 again from scratch

--- main.c
+++ main.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
 
-void zeroInt(int *x);
 void mean(int *a, int *b, int *c);
 
 int main(){
-    int x = 10, y = 3, z = 7, *a, *b, *c, d, e, f;
+    int *a, *b, *c, x = 15, y = 65, z = 26;
     
     a = &x;
     b = &y;
     c = &z;
-    d = x;
-    e = y;
-    f = z;
-    mean(a, b, c);  
-    printf("\nMean of %d, %d, %d is %d. The other two variables zero'd are %d & %d\n", d, e, f, x, y, z);
+    mean(a, b, c);
+    printf("\nx becomes mean %d, y becomes %d, z becomes %d\n", *a, *b, *c);
     
     return 0;
 }
 
 void mean(int *a, int *b, int *c){
-    int mean;
+    int sum, mean;
     
     mean = (*a + *b + *c)/3;
     *a = mean;
